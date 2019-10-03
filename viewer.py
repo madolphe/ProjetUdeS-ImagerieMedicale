0a33cf7766dd6b41cf4b343e62be63ee1b76d079
madolphe <madolphe@ensc.fr>
2019-10-03 11:57:59 -0400

    maj pipfile

--- viewer.py
+++ viewer.py
@@ -1,3 +1,22 @@
 import numpy
+import nibabel as nib
+import matplotlib.pyplot as plt
+import holoviews as hv
+hv.extension('bokeh')
+
+def viewer(I, mode='axial'):
+    """
+    I: image 3D à considérer
+    mode: coupe souhaitée, 3 possibilités -> 'axial': vue du dessus, [:, :, image souhaitée, couleur]
+                                             'sagittale': vue de coté [image souhaitée ,:, :, couleur]
+                                             'coronale': vue de devant [:, image souhaitée, :, couleur]
+
+    :return:
+    """
+    img = nib.load(I).get_data()
+    print(img.shape)
+    return
+
+
 if __name__ == '__main__':
-    pass
+    viewer('Data_MiseEnForme/IRM/Heart/PetitAxe/Slice06.nii')
